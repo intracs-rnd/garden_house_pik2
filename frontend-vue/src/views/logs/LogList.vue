d2f906d9a6a5868efcee61ad326561b73ebbb238
andhikarpp-dev <andhika.rizkipp@gmail.com>
2026-07-16 11:33:05 +0700

    LogList.vue

---
+++ frontend-vue/src/views/logs/LogList.vue
@@ -0,0 +1,21 @@
+﻿<template>
+  <div class="log-list">
+    <h1>Log Error</h1>
+    <p>Error log list will be displayed here.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogList',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.log-list {
+  padding: 20px;
+}
+</style>
